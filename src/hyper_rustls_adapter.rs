a2a2d1f55f08d7b7b4a785073108fa70ff2a189d
mautamu <kennroo22@gmail.com>
2025-01-28 13:34:40 -0600

    Update hyper_rustls_adapter.rs

--- src/hyper_rustls_adapter.rs
+++ src/hyper_rustls_adapter.rs
@@ -182,7 +182,7 @@ impl Adapter for HyperRustlsAdapter {
             .await
             .map_err(|e| Error::new_from(ErrorKind::ExchangeFailure, e))?;
         if !response.status().is_success() {
-            dbg!(&response.body());
+            dbg!(&response);
             return Err(Error::new(ErrorKind::ExchangeError(
                 response.status().as_u16(),
             )));
